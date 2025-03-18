58b22974f2fb5e3f40d4243e393c169de12d46af
Rayishin <ilya.ikanov@bk.ru>
2025-03-18 20:26:44 +0300

    Add project files.

---
+++ Preprocessor directives/Preprocessor directives.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+
+#define MODE 1
+
+#if !defined MODE
+#error "You must define MODE before compilation"
+#endif
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+#if MODE == 0
+	std::cout << "Working in training mode" << std::endl;
+
+#elif MODE == 1
+	std::cout << "Working in combat mode" << std::endl;
+
+	int number1, number2;
+
+	std::cout << "Enter the number 1: ";
+	std::cin >> number1;
+
+	std::cout << "Enter the number 2: ";
+	std::cin >> number2;
+
+	std::cout << "The result of the addition: " << add(number1, number2) << std::endl;
+
+#else
+std::cout << "Unknown mode. Shutdown." << std::endl;
+#endif
+
+	return EXIT_SUCCESS;
+}
